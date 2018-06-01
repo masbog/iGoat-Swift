25c7630165ca674b02788a254e7e4fee1e3b7b36
Augusta Bogie <me@masbog.com>
2018-06-02 06:41:12 +0700

    adding jailbreak detection library isJB

---
+++ iGoat-Swift/iGoat-Swift/isJB.h
@@ -0,0 +1,14 @@
+//
+//  isJB.h
+//  isJB
+//
+//  Created by Augusta Bogie on 14/12/17.
+//  Copyright © 2017 Augusta Bogie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface isJB : NSObject
+- (NSString *)checkVersion;
+- (NSDictionary *)isJailbrokenDevice;
+@end
